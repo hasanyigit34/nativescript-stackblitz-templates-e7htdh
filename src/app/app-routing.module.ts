8398aa33c0fd6f5c5f0ea0b6cc7d03b1945d083c
hasanyigit34 <63562154+hasanyigit34@users.noreply.github.com>
2022-08-22 19:21:08 +0300

    Add files via upload

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,13 +2,24 @@ import { NgModule } from '@angular/core'
 import { Routes } from '@angular/router'
 import { NativeScriptRouterModule } from '@nativescript/angular'
 
-import { ItemsComponent } from './item/items.component'
-import { ItemDetailComponent } from './item/item-detail.component'
+import { LoginComponent } from './login/login.component'
+import { ItemsComponent } from './change-language/items.component'
+import { ChangeLanguageComponent } from './change-language/change-language.component'
+import { Step1Component } from './onboarding/step1'
+import { Step2Component } from './onboarding/step2'
+import { Step4Component } from './onboarding/step4'
+import { Step3Component } from './onboarding/step3'
 
 const routes: Routes = [
-  { path: '', redirectTo: '/items', pathMatch: 'full' },
+  { path: '', redirectTo: '/onboarding/step1', pathMatch: 'full' },
+  { path: 'login', component: LoginComponent },
   { path: 'items', component: ItemsComponent },
-  { path: 'item/:id', component: ItemDetailComponent },
+  { path: 'change-language', component: ChangeLanguageComponent },
+  { path: 'onboarding/step1', component: Step1Component },
+  { path: 'onboarding/step2', component: Step2Component },
+  { path: 'onboarding/step3', component: Step3Component },
+  { path: 'onboarding/step4', component: Step4Component },
+ 
 ]
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,13 +3,22 @@ import { NativeScriptModule } from '@nativescript/angular'
 
 import { AppRoutingModule } from './app-routing.module'
 import { AppComponent } from './app.component'
-import { ItemsComponent } from './item/items.component'
-import { ItemDetailComponent } from './item/item-detail.component'
+import { ItemsComponent } from './change-language/items.component'
+import { ChangeLanguageComponent } from './change-language/change-language.component'
+import { LoginComponent } from './login/login.component'
+import { Step1Component } from './onboarding/step1';
+import { Step2Component } from './onboarding/step2';
+import { Step3Component } from './onboarding/step3';
+import { Step4Component } from './onboarding/step4';
 
 @NgModule({
   bootstrap: [AppComponent],
   imports: [NativeScriptModule, AppRoutingModule],
-  declarations: [AppComponent, ItemsComponent, ItemDetailComponent],
+  declarations: [AppComponent,LoginComponent, ItemsComponent, ChangeLanguageComponent,
+  Step1Component,
+  Step2Component,
+  Step3Component,
+  Step4Component],
   providers: [],
   schemas: [NO_ERRORS_SCHEMA],
 })
